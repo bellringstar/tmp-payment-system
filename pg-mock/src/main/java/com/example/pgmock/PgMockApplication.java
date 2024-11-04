427ea4a414e5161715f9872abc2f469e4f741d37
bellringstar <starringbell@gmail.com>
2024-11-04 19:59:04 +0900

    Init project

---
+++ pg-mock/src/main/java/com/example/pgmock/PgMockApplication.java
@@ -0,0 +1,13 @@
+package com.example.pgmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgMockApplication.class, args);
+    }
+
+}
---
+++ pg-mock/src/test/java/com/example/pgmock/PgMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pgmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgMockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
